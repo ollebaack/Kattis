2b2c0a98c57192ca2830f6608d4f7186b85b826a
Olle Back <ollebaack@gmail.com>
2017-04-13 18:36:34 +0200

    hrt

---
+++ I_repeat_myself/I_repeat_myself/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <array>
+using namespace std;
+
+int amountOfVoc(int index)
+{
+	vector<string> word;
+
+	for (int i = 0; i < index; i++)
+	{
+		cin >> word[i];
+		cout << "\n";
+	}
+
+	return 0;
+}
+
+
+
+int main() {
+	int amount;
+	cin >> amount;
+	amountOfVoc(amount);
+
+
+}
